e18d558f2e832f2880ec4a30b69a095e1cb6902f
wangban <yzbbanban@live.com>
2018-07-02 17:16:55 +0800

    创建服务

--- src/main/java/com/yzbbanban/controller/HelloController.java
+++ src/main/java/com/yzbbanban/controller/HelloController.java
@@ -1,18 +1,28 @@
 package com.yzbbanban.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by ban on 2018/7/2.
  */
 @RestController
 public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        System.out.println("ssssssssss");
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello,host: " + instance.getHost() + ",service_id: " + instance.getServiceId());
         return "hello world";
     }
 }
