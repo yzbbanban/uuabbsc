a5c4b4b54d8ca34c2e691b6307915dd7cf69c13e
wangban <yzbbanban@live.com>
2018-07-02 15:27:16 +0800

    创建服务

--- src/main/java/com/yzbbanban/UuabbApplication.java
+++ src/main/java/com/yzbbanban/UuabbApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UuabbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UuabbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UuabbApplication.class, args);
+    }
 }
